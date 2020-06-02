ba3813cac20ef42a61f6ce3fa60f4a7efbeb1da2
Adam Fowler <adamfowler71@gmail.com>
2020-06-02 16:46:22 +0100

    Version 4.7.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-  name: "CloudWatch",
-  products: [
-      .library(name: "CloudWatch", targets: ["CloudWatch"]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
-  ],
-  targets: [
-      .target(name: "CloudWatch", dependencies: ["AWSSDKSwiftCore"]),
-  ]
+    name: "CloudWatch",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "CloudWatch", targets: ["CloudWatch"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+    ],
+    targets: [
+        .target(name: "CloudWatch", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/CloudWatch"),
+    ]
 )
